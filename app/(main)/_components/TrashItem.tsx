88b0c251896c95b4c27063f674a9f46e36d25976
aamigo <aamigodeveloper@gmail.com>
2024-06-20 19:20:48 +0330

    fixing the delete redirect

--- app/(main)/_components/TrashItem.tsx
+++ app/(main)/_components/TrashItem.tsx
@@ -48,15 +48,13 @@ const TrashItem = ({ isMobile }: Props) => {
   const onDelete = (documentId: Id<"documents">) => {
     const promise = deleteDocument({
       id: documentId,
-    })
+    }).then(() => router.push("/documents"))
 
     toast.promise(promise, {
       loading: "Deleting note...",
       success: "Note Deleted",
       error: "Failed to delete note.",
     })
-    
-    router.push("/documents")
   }
 
   if (documents === undefined) {
